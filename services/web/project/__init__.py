af287ff5ce99b815fa371084746437f9197e035d
yechanj <wn9392@gmail.com>
2023-01-19 16:36:35 +0900

    [ADD] Basic Routing implemented

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, redirect
+import random
+
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -17,10 +19,76 @@ class User(db.Model):
     def __init__(self, email):
         self.email = email
         
+nextId = 4
 
-@app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+# List of dictionary
+topics = [
+    {'id': 1, 'title': 'html', 'body': 'html is...'},
+    {'id': 2, 'title': 'css', 'body': 'css is...'},
+    {'id': 3, 'title': 'javascript', 'body': 'javascript is...'},
+]
 
-if __name__ == '__main__':
-	app.run(debug=True)
+def template(contents, content):
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a><h1>
+            <ol>
+                {contents}
+            </ol>
+            {content}
+            <ul>
+                <li><a href="/create/">create</a></li>
+            </ul>
+        </body>
+    </html>
+    '''
+
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + \
+            f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
+
+
+@app.route('/')
+def index():
+    return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
+
+@app.route('/read/<int:id>/')
+def read(id):
+    title = ''
+    body = ''
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+    print(title, body)
+    return template(getContents(), f'<h2>{title}</h2>{body}')
+
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        content = '''
+            <form action="/create/" method="POST">
+                <p><input type="text" name="title" placeholder="title">></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit" value="create"></p>
+            </form>
+        '''
+        return template(getContents(), content)
+    elif request.method == 'POST':
+        global nextId #메서드 내부에서 전역변수 수정하기 위함.
+        title = request.form['title']
+        body = request.form['body']
+        newTopic = {'id': nextId, 'title': title, 'body': body}
+        topics.append(newTopic)
+        url = '/read/'+str(nextId)+'/'
+        nextId = nextId + 1
+        return redirect(url)
+
+
+app.run(port=5001, debug=True)
